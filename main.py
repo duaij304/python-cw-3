61b90d2fe183bd6e89ca0d73d2f518d97f097191
duaij304 <69263437+duaij304@users.noreply.github.com>
2023-08-28 19:06:48 +0300

    finshed

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# write your code here
+#الجزء الاول 
+
+favorite_animals = ['lion', 'cat', 'monkey', 'rabbit']
+
+print(favorite_animals)
+
+print(favorite_animals[1])
+
+favorite_animals.remove("monkey")
+print(favorite_animals)
+#الجزء الثاني
+
+favorite_animals.append('camel')
+
+for animal in favorite_animals:
+    print("I love  " + animal)
+
+numbers = [1, 2, 3, 4, 5]
+numbers_sum = 0
+#الجزء الثالث 
+for number in numbers:
+    numbers_sum += number
+print("Total :", numbers_sum)
